008c9b6a1bc6de2ac3ee31a12caaa82c8fe04a32
Oscar Chapa <tutoschapa@gmail.com>
2022-06-22 09:33:46 -0600

    Clase de numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,23 @@
+(()=> {
+  let productPrice = 100;
+  productPrice = 12;
+
+  let costumerAge: number = 30;
+
+  let productStock: number = 100;
+  if (productStock > 10) {
+    console.log('entro');
+  }
+
+  let discount = parseFloat('hola');
+  discount //NaN es number
+
+  let hex = 0xf3f3f3;
+  hex // Hexadecimal es number
+
+  let bin = 0b011001010;
+  bin // Binario es number
+
+  const myNumber: Number = 10; // se tipa como la clase Number
+
+})()
