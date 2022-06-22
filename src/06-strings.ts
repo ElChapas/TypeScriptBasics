4647aabe21cc7ecc21d3bd54b6ed13a141f7a028
Oscar Chapa <tutoschapa@gmail.com>
2022-06-22 10:00:30 -0600

    Clase strings

---
+++ src/06-strings.ts
@@ -0,0 +1,10 @@
+(() => {
+  let productTitle = 'Laptop'
+  let productDescription: string ="I't a new computer";
+  let price: number = 200;
+  const summary = `
+    Title: ${productTitle}
+    Description: ${productDescription}
+    Pice: ${price}
+  `;
+})()
